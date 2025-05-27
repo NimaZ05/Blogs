1586dcbf352cf242255cb6043e0d8b3e84082498
NimaZ <nimaz2138487@gmail.com>
2025-05-27 23:08:52 +0330

    Initial commit

---
+++ pages/views.py
@@ -0,0 +1,26 @@
+from django.shortcuts import render
+from django.views.generic import TemplateView
+
+
+class HomePageView(TemplateView):
+    template_name = 'pages/home.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        return context
+
+
+class AboutPageView(TemplateView):
+    template_name = 'pages/about.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        return context
+
+
+class ContactPageView(TemplateView):
+    template_name = 'pages/contact.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        return context
